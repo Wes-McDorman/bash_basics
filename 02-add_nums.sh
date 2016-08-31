5085ea88930bc6eed3c202aac39bd5c1e3f2ca66
Wes-McDorman <wmcdorma@ggc.edu>
2016-08-30 23:33:06 -0400

    second oops edit for Assignment 2

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Enter your width number"
+echo "Enter your blarggg number"
 read width
-echo "Enter your height number"
+echo "Enter your  barg number"
 read height
 
 
 sum=$(( width*height ))
 
-echo "The total pixels are $sum"
+echo "The total blard are $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
